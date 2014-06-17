16edfa43ba18c73850a5aeb1d0fc4899ad96d051
Bryan Hoyle <starfoxprime@gmail.com>
2014-06-17 19:57:23 -0400

    Basic stuffs for markov

--- src/twitter_zombies/core.clj
+++ src/twitter_zombies/core.clj
@@ -1,7 +1,14 @@
 (ns twitter-zombies.core
-  (:require [twitter-zombies.twitter :as twitter]))
+  (:require [twitter-zombies.twitter :as twitter]
+            [twitter-zombies.graph :as mc]
+            [twitter-zombies.resources :as res]))
 
 (defn -main
   [& args]
   (do (twitter/tweet "Twitter/tweet function call test.")
-  (System/exit 0)))
+      (System/exit 0)))
+
+
+
+(def random-text 
+  (mc/add-text {} (slurp (res/resource-file "stallman.txt"))))
