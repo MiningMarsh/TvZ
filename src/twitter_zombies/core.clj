6fe5dee00b337f33ed76e6a4d85160c4b216ea66
Bryan Hoyle <starfoxprime@gmail.com>
2014-06-18 16:49:01 -0400

    better main

--- src/twitter_zombies/core.clj
+++ src/twitter_zombies/core.clj
@@ -8,16 +8,16 @@
   (:import (twitter.callbacks.protocols AsyncStreamingCallback)))
 
 (def ^:dynamic
-     *custom-streaming-callback*
-     (AsyncStreamingCallback. (callback-factory (comp println :text))
-                      (comp println response-return-everything)
-                  exception-print))
+  *custom-streaming-callback*
+  (AsyncStreamingCallback. (callback-factory (comp println :text))
+                           (comp println response-return-everything)
+                           exception-print))
 
-(statuses-filter :params {:track "yolo"}
-         :oauth-creds my-creds
-         :callbacks *custom-streaming-callback*)
+
 
 (defn -main
   "Print all incoming tweets from search."
   [& args]
-  (println "Done"))
+  (statuses-filter :params {:track (first args)}
+                   :oauth-creds (get-creds)
+                   :callbacks *custom-streaming-callback*))
